c16264015118ab2af90a0d4cd8e5ad98d864440a
firebird75 <firebird75@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-18 17:44:01 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wats/tags/1.0@127130 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wats.php
+++ wats.php
@@ -9,28 +9,15 @@ Author URI: http://www.lautre-monde.fr
 */
 
 /*
-1/ License terms :
-- This release is provided for free. In exchange, you must keep the backlinks added by the plugin 
-in the footer of the ticket pages on the frontend.
-- You cannot redistribute this software without my written agreement.
-- You cannot modify it for your own needs or third party needs without my written agreement.
-- You cannot reuse parts of the code without my written agreement.
-- This is not an open source software.
-- This software is provided "As Is". I don't provide any warranty of any kind whether express or implied.
-I give no warranty regarding the quality, reliability, timeliness or security of the service and software.
-I will not endorse any responsibility for any damage or problem that could happen as part of the usage of
-this software. This software has been developped by a human so it is not error free.
-=> By installing and using this software, you fully accept all the terms of the license without any restriction.
-
-2/ Release history :
+1/ Release history :
 - V1.0 (15/06/2009) : Initial release
 
-3/ Plugin description :
+2/ Plugin description :
 This plugin  adds to wordpress the functionnalities of a ticket system. This allows users to submit tickets to
 report problems or get support on whatever you want. You can customize the status, priority and type of
 the ticket.
 
-4/ Credits :
+3/ Credits :
 WATS uses the following scripts :
 - Editable from Arash Karimzadeh (arashkarimzadeh.com)
 - jQuery
