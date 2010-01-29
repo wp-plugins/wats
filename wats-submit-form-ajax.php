3610334dfb309e319e48d097e4f3ae43a59e4db8
firebird75 <firebird75@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-29 20:46:18 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wats/tags/1.0.44@199625 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wats-submit-form-ajax.php
@@ -0,0 +1,29 @@
+<?php
+?>
+jQuery(document).ready(function() {
+    jQuery('#submit_ticket').click(function() {
+			jQuery('#submit_ticket').attr('disabled','disabled');
+			var view = 1;
+			wats_loading(document.getElementById("resultticketsubmitform"),watsmsg[1]);
+			var name = jQuery('#name').val();
+			var email = jQuery('#email').val();
+			var url = jQuery('#url').val();
+			var ticket_title = jQuery('#ticket_title').val();
+			var ticket_content = jQuery('#ticket_content').val();
+			var idtype = jQuery('#wats_select_ticket_type option:selected').val();
+			var idpriority = jQuery('#wats_select_ticket_priority option:selected').val();
+			var idstatus = jQuery('#wats_select_ticket_status option:selected').val();
+			var categoryfilter = jQuery('#categoryfilter').val();
+			var categorylistfilter = jQuery('#categorylistfilter').val();
+			jQuery.post(ajaxurl, {action:"wats_ticket_submit_form_ajax_processing", _ajax_nonce:jQuery("#_wpnonce_ticket_submit_form").val(), view:view, name:name, email:email, url:url, ticket_title:ticket_title, ticket_content:ticket_content, idtype:idtype, idpriority:idpriority, idstatus:idstatus, categoryfilter:categoryfilter, categorylistfilter:categorylistfilter},
+			function(res)
+			{
+				wats_stop_loading(document.getElementById("resultticketsubmitform"),res);
+				jQuery('#submit_ticket').removeAttr('disabled');
+			});
+		
+		return false;
+	});
+
+	return false;
+});
